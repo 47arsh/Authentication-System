1556c24a0ae0909a4d994eb00b81a32f86e32602
47arsh <arxhx7@gmail.com>
2026-02-09 13:51:34 +0530

    feat: add signup API with password hashing and duplicate user check

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,45 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel.js"
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import { hash } from "crypto";
+
+connectDB();
+
+export async function POST(request : NextRequest){
+    try {
+        const reqBody = await request.json()
+        const {username, email, password} = reqBody;
+
+        const existingUser = await User.findById({email});
+        if(existingUser){
+            return NextResponse.json(
+                {error : "User already exists"},
+                {status : 400}
+            )
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new User({
+            username : username,
+            email : email,
+            password : hashedPassword
+        })
+
+        const savedUser = await newUser.save();
+
+        return NextResponse.json({
+            message : "user created successfully",
+            success : true,
+            user : savedUser
+        })
+
+
+    } catch (error : any) {
+        return NextResponse.json(
+            {error : error.message},
+            {status : 500}
+        )
+    }
+}
